69ee5827b3e7aa670b83fd5907eca28546315930
EC2 Default User <ec2-user@ip-172-31-18-14.us-east-2.compute.internal>
2019-07-19 13:11:58 +0000

    initial commit

--- BillSpliter.py
+++ BillSpliter.py
@@ -1,8 +1,20 @@
 import math
-
-# subTotal = int(subTotal) 
-total = input("What is the total of your bill?\n")
+ 
+ 
+subTotal = input("What is the total of your bill?\n")
 people = input("How many people are you dining with? Include yourself.\n")
-totalCost = int(total) / int(people)
-print ("The cost of your bill pre-tips is " + str(totalCost) + ".")
+tip = input("Would you like to provide a gratituity?\n")
+
+
+if tip[0].lower() == "y":
+    chosenTipPercentage = input("What percentage for gratituity would you like to give? Example: 15% is 15.\n")
+    numericalTip = (int(chosenTipPercentage)/100)+1 
+    tipTotal = int(subTotal) * int(numericalTip)
+    finalTotal = int(subTotal) + int(tipTotal)
+    CostPerPerson = int(finalTotal)/int(people)
+    print ("Each person needs to pay $" + str(CostPerPerson) + ".")
+else: 
+    print("Okay, Mr. Krabs. We see you.")
+
+
 
